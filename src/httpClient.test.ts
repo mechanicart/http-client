abb4f536343ea3bf07418d1cff474a1937956e95
Tikhomirov Artem Igorevich <tikhomirov.artem.i@gmail.com>
2022-02-07 14:00:00 +0300

    update tests

--- src/httpClient.test.ts
+++ src/httpClient.test.ts
@@ -7,20 +7,33 @@ type Todo = {
   completed: boolean;
 }
 
+async function getUser(id: number) {
+  fetch(`/users/${id}`)
+}
+
 describe('httpClient()', () => {
-  it('make a pure request', async () => {
+  it.skip('make a pure request', async () => {
     const response = await httpClient<Todo[]>("https://jsonplaceholder.typicode.com/todos");
     expect(response).toEqual({ type: 'foo', payload: 'bar' });
   });
 
-  it('make a GET request', async () => {
-    const response = await get<{ id: number }>(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    expect(response).toEqual({ type: 'foo', payload: 'bar' });
+  it.skip('make a GET request', async () => {
+    await get<Todo[]>("https://jsonplaceholder.typicode.com/posts");
   });
 
-  it(' 2 + 2 equal 4', () => {
-    expect(2 + 2).toEqual(4)
-  });
+  it('calls the right route', async () => {
+    // Create an empty mock function which just returns nothing
+    const mockedFetch = jest.fn()
+    // Set the global fetch to be this function
+    global.fetch = mockedFetch
+    const res = await getUser(12)
+    expect(mockedFetch).toHaveBeenCalledWith('/users/12')
+  })
+
+  it('calls with a spyOn', async () => {
+    jest.spyOn(global, 'fetch')
+    await getUser(12)
+    expect(global.fetch).toHaveBeenCalledWith('/users/12')
+  })
+
 });
